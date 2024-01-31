4b2e835981567797585218f74d523463a2b46895
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-01 01:39:21 +0300

    start header test variant

--- src/main/java/com/iw/App.java
+++ src/main/java/com/iw/App.java
@@ -3,13 +3,22 @@ package com.iw;
 import com.iw.page.HomePage;
 import io.javalin.Javalin;
 
-import static j2html.TagCreator.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class App {
     public static void main(final String[] args) {
         Javalin.create()
-                .get("/", ctx -> ctx.html(new HomePage().content()))
+                .get("/", ctx -> ctx.html(new HomePage(persons()).content()).status(202))
                 .error(404, ctx -> ctx.result("Generic 404 message"))
                 .start(8080);
+    }
+
+    private static List<String> persons() {
+        return new ArrayList<>() {{
+            add("Arthas");
+            add("Glad Valakas");
+            add("Kuplinov");
+        }};
     }
 }
--- src/main/java/com/iw/page/HomePage.java
+++ src/main/java/com/iw/page/HomePage.java
@@ -2,14 +2,44 @@ package com.iw.page;
 
 import com.iw.Page;
 
+import java.util.List;
+
 import static j2html.TagCreator.*;
 
 public final class HomePage implements Page {
+
+    private final List<String> persons;
+
+    public HomePage(List<String> persons) {
+        this.persons = persons;
+    }
+
     @Override
     public String content() {
         return html(
                 head(title("Blunts2")),
-                body(j2html.TagCreator.main(h1("Blunts2")))
+                body(
+                        main(
+                                section(
+                                        header(
+                                                h1("Blunts2"),
+                                                p(join(
+                                                        b("Blunts2"),
+                                                        "is statistic with the number of failed attempts to complete the game.")
+                                                ),
+                                                nav(
+                                                        ul(
+                                                                each(persons, person ->
+                                                                        li(
+                                                                                a(person).withHref("/" + person)
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        )
+                                )
+                        )
+                )
         ).render();
     }
 }
