f96a451f785542bdf4aa70f9912746d277c16559
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-01 00:41:12 +0300

    return html

--- src/main/java/com/iw/App.java
+++ src/main/java/com/iw/App.java
@@ -2,10 +2,21 @@ package com.iw;
 
 import io.javalin.Javalin;
 
+import static j2html.TagCreator.*;
+
 public final class App {
     public static void main(final String[] args) {
         Javalin.create()
-                .get("/", ctx -> ctx.result("Hello world 2 !"))
+                .get("/", ctx -> ctx.html(content()))
+                .get("/404", ctx -> ctx.status(404))
+                .get("/502", ctx -> ctx.status(502))
                 .start(8080);
+    }
+
+    private static String content() {
+        return html(
+                head(title("Blunts2")),
+                body(j2html.TagCreator.main(h1("Blunts2")))
+        ).render();
     }
 }
