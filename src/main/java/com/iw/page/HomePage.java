f855d633d9fbe1dfbb5c2c507f552fd15a691eac
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-01 22:28:55 +0300

    header and footer

--- src/main/java/com/iw/page/HomePage.java
+++ src/main/java/com/iw/page/HomePage.java
@@ -18,29 +18,33 @@ public final class HomePage implements Page {
     public String content() {
         return html(
                 head(
+                        meta().withCharset("UTF-8"),
+                        meta().withName("viewport").withContent("width=device-width, initial-scale=1.0"),
+                        meta().withName("keywords").withContent("streamers, statistic, rating, twitch"),
+                        meta().withName("description").withContent("Streamers failed statistic"),
                         title("Blunts2"),
-                        link().withRel("stylesheet").withHref("css/simple.min.css")
+                        link().withRel("stylesheet").withHref("css/simple.min.css"),
+                        link().withRel("icon").withType("image/png").withHref("images/logo/logo_32.png")
                 ),
                 body(
-                        main(
-                                section(
-                                        header(
-                                                h1("Blunts2"),
-                                                p(join(
-                                                        b("Blunts2"),
-                                                        "is statistic with the number of failed attempts to complete the game.")
-                                                ),
-                                                nav(
-                                                        ul(
-                                                                each(persons, person ->
-                                                                        li(
-                                                                                a(person).withHref("/" + person.replaceAll("\\s", "").toLowerCase())
-                                                                        )
-                                                                )
-                                                        )
-                                                )
-                                        )
+                        header(
+                                img().withSrc("images/logo/logo_96.png").withWidth("70"),
+                                h1("Blunts2"),
+                                p(join(
+                                        b("Blunts2"),
+                                        "is statistic with the number of failed attempts to complete the game.")
                                 )
+                        ),
+                        main(
+                                p("content")
+                        ),
+                        footer(
+                                p(join(
+                                        "Made by ",
+                                        a("@Ig-v-k").withHref("https://github.com/Ig-v-k"),
+                                        ", 2024"
+                                )),
+                                nav(ul(li(a("GitHub").withHref("https://github.com/Ig-v-k/blunts2"))))
                         )
                 )
         ).render();
