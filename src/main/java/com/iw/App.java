81f1c165ef76c282567bd5c7dd356d7537052275
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-02 20:54:05 +0300

    users route

--- src/main/java/com/iw/App.java
+++ src/main/java/com/iw/App.java
@@ -2,6 +2,7 @@ package com.iw;
 
 import com.iw.page.HomePage;
 import io.javalin.Javalin;
+import io.javalin.http.HttpStatus;
 import io.javalin.http.staticfiles.Location;
 
 import java.util.ArrayList;
@@ -16,14 +17,11 @@ public final class App {
     }};
 
     public static void main(final String[] args) {
-        final Javalin app = Javalin.create(cfg ->
-                        cfg.staticFiles.add("/assets", Location.CLASSPATH))
-                .get("/", ctx -> ctx.html(new HomePage(persons).content()).status(202))
-                .error(404, ctx -> ctx.result("Generic 404 message"));
-        for (String person : persons) {
-            final String formatted = person;
-            app.get("/" + person, ctx -> ctx.html(new PersonPage(person).content()).status(202));
-        }
-        app.start(8080);
+        Javalin.create(cfg -> cfg.staticFiles.add("/assets/public", Location.CLASSPATH))
+                .get("/", ctx -> ctx.html(new HomePage(persons).content()))
+                .get("/users", ctx -> ctx.html(new PersonsPage().content()))
+                .get("/users/{id}", ctx -> ctx.html(new PersonPage(ctx.pathParam("id")).content()))
+                .error(HttpStatus.NOT_FOUND, ctx -> ctx.result("404. Page not found"))
+                .start(8080);
     }
 }
