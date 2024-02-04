4b4a191cc1f6eb52ce5f8f0603736b03fa786835
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-04 15:06:09 +0300

    person

---
+++ src/main/java/com/iw/Person.java
@@ -0,0 +1,12 @@
+package com.iw;
+
+public interface Person {
+    int id();
+    int code();
+    String firstname();
+    String lastname();
+    int blunts();
+    int period();
+    String nickname();
+    String link();
+}
