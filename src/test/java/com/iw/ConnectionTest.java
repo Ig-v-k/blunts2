afdc8116c881811cffee65d61fa5c3feec8bd8b0
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-04 14:37:11 +0300

    test database data

--- src/test/java/com/iw/ConnectionTest.java
+++ src/test/java/com/iw/ConnectionTest.java
@@ -3,9 +3,7 @@ package com.iw;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,10 +13,20 @@ public class ConnectionTest {
     public void initialize() throws SQLException {
         con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "postgres");
         con.setSchema("public");
+        con.setAutoCommit(false);
     }
 
     @Test
     public void mainSchema() throws SQLException {
         assertThat(con.getSchema()).isEqualTo("public");
+    }
+
+    @Test
+    public void row() throws SQLException {
+        final Statement st = con.createStatement();
+        final ResultSet rs = st.executeQuery("SELECT * FROM person");
+        rs.next();
+        final int row = rs.getRow();
+        assertThat(row).isEqualTo(1);
     }
 }
