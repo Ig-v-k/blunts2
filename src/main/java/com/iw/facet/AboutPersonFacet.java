fbd81516ba8761a8079e2bd99eb3ee3c3254d98f
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-06 21:31:08 +0300

    extra section dividers

--- src/main/java/com/iw/facet/AboutPersonFacet.java
+++ src/main/java/com/iw/facet/AboutPersonFacet.java
@@ -3,12 +3,12 @@ package com.iw.facet;
 import com.iw.Facet;
 import com.iw.Person;
 import j2html.tags.Tag;
-import j2html.tags.specialized.SectionTag;
+import j2html.tags.specialized.MainTag;
 
 import static j2html.TagCreator.h1;
-import static j2html.TagCreator.section;
+import static j2html.TagCreator.main;
 
-public final class AboutPersonFacet implements Facet<SectionTag> {
+public final class AboutPersonFacet implements Facet<MainTag> {
 
     private final Person person;
 
@@ -17,8 +17,8 @@ public final class AboutPersonFacet implements Facet<SectionTag> {
     }
 
     @Override
-    public Tag<SectionTag> tag() {
+    public Tag<MainTag> tag() {
         final String nickname = person.nickname();
-        return section(h1(nickname));
+        return main(h1(nickname));
     }
 }
