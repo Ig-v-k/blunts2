3a1bc27c339518144d3c083c24376cf7e224c9e1
Igor Wysocki <lgorvysocky@gmail.com>
2024-02-10 18:40:57 +0300

    full test game

--- src/test/java/com/iw/GameTest.java
+++ src/test/java/com/iw/GameTest.java
@@ -3,6 +3,8 @@ package com.iw;
 import com.iw.game.SimpleGame;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public final class GameTest extends ContainerAbstract {
@@ -11,5 +13,24 @@ public final class GameTest extends ContainerAbstract {
         final int person = 0;
         final Game game = new SimpleGame(container, 0);
         assertThat(game.person()).isEqualTo(person);
+    }
+
+    @Test
+    public void title() {
+        final Game game = new SimpleGame(container, 0);
+        assertThat(game.title()).isNotEmpty();
+    }
+
+    @Test
+    public void blunts() {
+        final Game game = new SimpleGame(container, 0);
+        assertThat(game.blunts()).isGreaterThanOrEqualTo(0);
+    }
+
+    @Test
+    public void attempts() {
+        final Game game = new SimpleGame(container, 0);
+        final List<Attempt> attempts = new SimpleAttempts(container, game.id()).list();
+        assertThat(attempts).isNotEmpty();
     }
 }
